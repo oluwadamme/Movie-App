7d0c18fe106ed19c3e39c7061c2f609048cbfaec
Damilola Adeniyi <adeniiyidamilola246@gmail.com>
2024-02-02 02:50:18 +0100

    convert model using type converters

--- app/src/main/java/com/example/movieapp/data/db/TMDBDatabase.kt
+++ app/src/main/java/com/example/movieapp/data/db/TMDBDatabase.kt
@@ -2,11 +2,15 @@ package com.example.movieapp.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.movieapp.data.model.Movie
+import com.example.movieapp.data.model.MovieTitle
+import com.example.movieapp.data.model.MovieTitleTypeConverter
 import com.example.movieapp.data.model.People
 
 
 @Database(entities = [Movie::class,People::class], version = 1)
+@TypeConverters(MovieTitleTypeConverter::class)
 abstract class TMDBDatabase : RoomDatabase(){
 
 abstract fun movieDao():MyDao
--- app/src/main/java/com/example/movieapp/data/model/People.kt
+++ app/src/main/java/com/example/movieapp/data/model/People.kt
@@ -2,7 +2,11 @@ package com.example.movieapp.data.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
+import com.google.gson.reflect.TypeToken
 
 @Entity(tableName = "popular_people")
 data class People(
@@ -11,12 +15,33 @@ data class People(
     val id: Int,
 
     val name: String,
+
     @SerializedName("profile_path")
     val image: String,
+
     @SerializedName("known_for")
-    val movies: List<MovieTitle>
-)
+    @TypeConverters(MovieTitleTypeConverter::class)
+    val movies: List<MovieTitle>,
+
+    )
 
 data class MovieTitle(
     val title: String,
 )
+
+class MovieTitleTypeConverter {
+
+    @TypeConverter
+    fun fromJson(value: String): List<MovieTitle> {
+        val listType = object : TypeToken<List<MovieTitle>>() {}.type
+
+        return Gson().fromJson(value, listType)
+    }
+
+
+    @TypeConverter
+    fun toJson(value: List<MovieTitle>): String {
+
+        return Gson().toJson(value)
+    }
+}
